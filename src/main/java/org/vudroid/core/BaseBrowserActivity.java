274e3126ec389748be864a66a14c4097259e51b5
apevzner <alina.pevzner@gmail.com>
2012-12-12 18:25:20 +0400

    Added authors category

--- src/main/java/org/vudroid/core/BaseBrowserActivity.java
+++ src/main/java/org/vudroid/core/BaseBrowserActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,53 +16,67 @@ import org.vudroid.core.presentation.UriBrowserAdapter;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.HashMap;
+import java.util.Map;
 
-public abstract class BaseBrowserActivity extends Activity
-{
-    private BrowserAdapter adapter;
+import static android.R.layout;
+
+public abstract class BaseBrowserActivity extends Activity {
+    private BrowserAdapter browserAdapter;
+    String authorsList[] = {"Tolstoy", "Turgenev"};
+    private Map<Integer, String[]> authorsMap = new HashMap<Integer, String[]>();
+    private boolean isAuthorFileOpen = false;
+    private ListView authorsListView;
+
     private static final String CURRENT_DIRECTORY = "currentDirectory";
-    private final AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener()
-    {
-        @SuppressWarnings({"unchecked"})
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-        {
-            final File file = ((AdapterView<BrowserAdapter>)adapterView).getAdapter().getItem(i);
-            if (file.isDirectory())
-            {
-                setCurrentDir(file);
-            }
-            else
-            {
-                showDocument(file);
-            }
-        }
-    };
+    private final AdapterView.OnItemClickListener onItemClickListener =
+            new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    switch (adapterView.getId()) {
+                        case R.id.AuthorsList:
+                            int authorId = (int) ((AdapterView<ArrayAdapter>) adapterView).getAdapter().getItemId(i);
+                            ((AdapterView<ArrayAdapter>) adapterView).setAdapter(null);
+                            ((AdapterView<ArrayAdapter>) adapterView).setAdapter(new ArrayAdapter<String>(BaseBrowserActivity.this,
+                                    layout.simple_list_item_1, authorsMap.get(authorId)));
+                            isAuthorFileOpen = true;
+                            break;
+                        default:
+                            final File file = ((AdapterView<BrowserAdapter>) adapterView).getAdapter().getItem(i);
+                            if (file.isDirectory()) {
+                                setCurrentDir(file);
+                            } else {
+                                showDocument(file);
+                            }
+                            break;
+                    }
+                }
+            };
     private UriBrowserAdapter recentAdapter;
     private ViewerPreferences viewerPreferences;
     protected final FileFilter filter;
 
     private TabHost mTabHost;
 
-    public BaseBrowserActivity()
-    {
+    public BaseBrowserActivity() {
         this.filter = createFileFilter();
     }
 
     protected abstract FileFilter createFileFilter();
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browser);
         viewerPreferences = new ViewerPreferences(this);
         final ListView browseList = initBrowserListView();
         final ListView recentListView = initRecentListView();
+        final ListView authorListView = initAuthorsListView();
+        initAuthorsMap();
         mTabHost = (TabHost) findViewById(R.id.browserTabHost);
         mTabHost.setup();
         setupTab(browseList, "Browse");
         setupTab(recentListView, "Recent");
-        //android:background="@drawable/background"
+        setupTab(authorListView, "Authors");
     }
 
     private void setupTab(final View view, final String tag) {
@@ -70,7 +85,7 @@ public abstract class BaseBrowserActivity extends Activity
         Log.d("setupTab", "1!!!!!!!!!!!");
         TabHost.TabContentFactory tabContentFactory = new TabHost.TabContentFactory() {
             public View createTabContent(String s) {
-                return  view;
+                return view;
             }
         };
         Log.d("setupTab", "2!!!!!!!!!!!");
@@ -85,6 +100,7 @@ public abstract class BaseBrowserActivity extends Activity
         mTabHost.addTab(setContent);
         Log.d("setupTab", "end!!!!!!!!!!!");
     }
+
     private static View createTabView(final Context context, final String text) {
         Log.d("createTabView", "begin!!!!!!!!!!!");
         View view = LayoutInflater.from(context).inflate(R.layout.tabsbg, null);
@@ -97,48 +113,38 @@ public abstract class BaseBrowserActivity extends Activity
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState)
-    {
+    protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         final File sdcardPath = new File("/sdcard");
-        if (sdcardPath.exists())
-        {
+        if (sdcardPath.exists()) {
             setCurrentDir(sdcardPath);
-        }
-        else
-        {
+        } else {
             setCurrentDir(new File("/"));
         }
-        if (savedInstanceState != null)
-        {
+        if (savedInstanceState != null) {
             final String absolutePath = savedInstanceState.getString(CURRENT_DIRECTORY);
-            if (absolutePath != null)
-            {
+            if (absolutePath != null) {
                 setCurrentDir(new File(absolutePath));
             }
         }
     }
 
-    private ListView initBrowserListView()
-    {
+    private ListView initBrowserListView() {
         final ListView listView = new ListView(this);
-        adapter = new BrowserAdapter(this, filter);
-        listView.setAdapter(adapter);
+        browserAdapter = new BrowserAdapter(this, filter);
+        listView.setAdapter(browserAdapter);
         listView.setOnItemClickListener(onItemClickListener);
         listView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
         return listView;
     }
 
-    private ListView initRecentListView()
-    {
+    private ListView initRecentListView() {
         ListView listView = new ListView(this);
         recentAdapter = new UriBrowserAdapter();
         listView.setAdapter(recentAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @SuppressWarnings({"unchecked"})
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-            {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 showDocument(((AdapterView<UriBrowserAdapter>) adapterView).getAdapter().getItem(i));
             }
         });
@@ -146,29 +152,53 @@ public abstract class BaseBrowserActivity extends Activity
         return listView;
     }
 
-    private void showDocument(File file)
-    {
+    private ListView initAuthorsListView() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        final ListView listView = (ListView) layoutInflater.inflate(R.layout.authors, null, false);
+        listView.setAdapter(new ArrayAdapter<String>(this, layout.simple_list_item_1, authorsList));
+        listView.setOnItemClickListener(onItemClickListener);
+        authorsListView = listView;
+        return listView;
+    }
+
+    private void initAuthorsMap() {
+        String booksList0[] = {"book01", "book02"};
+        authorsMap.put(0, booksList0);
+        String booksList1[] = {"book11", "book12"};
+        authorsMap.put(1, booksList1);
+    }
+
+    private void showDocument(File file) {
         showDocument(Uri.fromFile(file));
     }
 
     protected abstract void showDocument(Uri uri);
 
-    private void setCurrentDir(File newDir)
-    {
-        adapter.setCurrentDirectory(newDir);
+    private void setCurrentDir(File newDir) {
+        browserAdapter.setCurrentDirectory(newDir);
         getWindow().setTitle(newDir.getAbsolutePath());
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState)
-    {
-        super.onSaveInstanceState(outState);
-        outState.putString(CURRENT_DIRECTORY, adapter.getCurrentDirectory().getAbsolutePath());
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && isAuthorFileOpen) {
+            authorsListView.setAdapter(null);
+            authorsListView.setAdapter(new ArrayAdapter<String>(this, layout.simple_list_item_1, authorsList));
+            isAuthorFileOpen = false;
+            return true;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(CURRENT_DIRECTORY, browserAdapter.getCurrentDirectory().getAbsolutePath());
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
         recentAdapter.setUris(viewerPreferences.getRecent());
     }
